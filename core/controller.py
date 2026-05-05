ca33dd1603684341c05abbf14e7fc9c4148efba8
Berigoo <muhamadrizqifajri2@gmail.com>
2026-05-05 22:03:09 +0700

    remoe print

--- core/controller.py
+++ core/controller.py
@@ -77,7 +77,7 @@ class ParkingOutController:
                     loop_sensor = inputs[self.modbus.reg_loop_sensor]
                     button = inputs[self.modbus.reg_button_ticket]
 
-                    print(f"[DEBUG controller] button state: {button}", flush=True)
+                    # print(f"[DEBUG controller] button state: {button}", flush=True)
                     # Logic: Loop Sensor
                     if loop_sensor == 1:
                         with self.state_lock:
